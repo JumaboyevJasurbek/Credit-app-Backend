93264fd1781363f4925c0eb7d7fba65cf8b3ff75
Jasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2022-12-23 01:26:11 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./modules/routes.js";
-// import errorhandler from "./middleware/errorhandler.js";
+import errorhandler from "./middleware/errorhandler.js";
 dotenv.config();
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-// app.use(errorhandler);
+app.use(errorhandler);
 app.all("/*", (req, res) => {
     res.status(404).json({
         message: req.url + "not found",
